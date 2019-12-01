92618bb3d622b0eab96237431705a6865bb92114
Enos Domingues <enosdomingues@gmail.com>
2019-12-01 09:03:24 -0300

    componentizado

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,30 +4,35 @@ import Comment from '../Comment/Comment'
 
 function Post({ author, avatar, date, content, comments }) {
   return (
+    <>
     <li>
       <div className="feed-component">
         <div className="feed-header">
-        <img src={avatar} /> 
-        <div className="content">
-          <strong>{author}</strong>
-          <small>{date}</small>
+          <img src={avatar} />
+          <div className="content">
+            <strong>{author}</strong>
+            <small>{date}</small>
+          </div>
         </div>
+        <div className="feed-text">
+          <span>{content}</span>
         </div>
-      <div className="feed-text">
-        <span>{content}</span>
-      </div>
-      <hr />
-      <ul>
-      {comments.map(com => 
-          <Comment 
-          key={com.id}
-          author={com.author.name}
-          avatar={com.author.avatar}
-          content={com.content}
+        <hr />
+        <div className="feed-header">
+      <ul id="ul-comment">
+        {comments.map(com =>
+          <Comment
+            key={com.id}
+            author={com.author.name}
+            avatar={com.author.avatar}
+            content={com.content}
           />)}
       </ul>
+    </div>
+
       </div>
     </li>
+    </>
   );
 }
 
